eecb9b1ffe786b1f5d2e5c0ce4c69661ff029c69
NaroArmenia <naroarmenia9@gmail.com>
2024-09-02 15:30:49 +0400

    Nor commit

---
+++ c_kod/dynamic7.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int parz(int* size){
+    int* parzarr = (int*)malloc(sizeof(int) * (*size));
+    int count = 0;
+
+    for(int i = 0; i < *size; ++i){
+        if(i <= 1){
+            continue;
+        }else if(i == 2 || i == 3){
+            parzarr[count] = i;
+            count++;
+            continue;
+        }
+        if(i % 2 == 0 || i % 3 == 0){
+            continue;
+        }
+        parzarr[count] = i;
+        count++;
+    }
+
+    parzarr = (int*)realloc(parzarr, sizeof(int) * count);
+
+    printf("ParzArr:");
+    for(int i = 0; i < count; ++i){
+        printf(" %d",parzarr[i]);
+    }
+    printf("\n");
+}
+
+int main(){
+    int num = 0;
+
+    printf("Enter the number:");
+    scanf("%d", &num);
+
+    parz(&num);
+}
